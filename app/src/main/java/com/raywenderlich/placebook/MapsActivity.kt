42ad34e92e4bff6f638ba349fc2947698dc15127
Luna <lunarotimas@gmail.com>
2021-10-05 03:18:54 -0400

    Ch15

--- app/src/main/java/com/raywenderlich/placebook/MapsActivity.kt
+++ app/src/main/java/com/raywenderlich/placebook/MapsActivity.kt
@@ -2,24 +2,37 @@ package com.raywenderlich.placebook
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.location.*
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PointOfInterest
 import com.raywenderlich.placebook.databinding.ActivityMapsBinding
+
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.FetchPhotoRequest
+import com.google.android.libraries.places.api.net.FetchPlaceRequest
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.raywenderlich.placebook.adapter.BookmarkInfoWindowAdapter
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var map: GoogleMap
+    private lateinit var placesClient: PlacesClient
     private lateinit var binding: ActivityMapsBinding
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
@@ -36,6 +49,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
 
         setupLocationClient()
+        setupPlacesClient()
     }
 
     /**
@@ -49,16 +63,32 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
+        map.setInfoWindowAdapter(BookmarkInfoWindowAdapter(this))
         getCurrentLocation()
+
+        map.setOnPoiClickListener {
+            displayPoi(it)
+        }
+
+
+    }
+
+    private fun setupPlacesClient() {
+        Places.initialize(
+            applicationContext,
+            getString(R.string.google_maps_key)
+        )
+        placesClient = Places.createClient(this)
     }
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
-        grantResults: IntArray) {
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == REQUEST_LOCATION) {
-            if (grantResults.size == 1 && grantResults[0] ==PackageManager.PERMISSION_GRANTED) {
+            if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 getCurrentLocation()
             } else {
                 Log.e(TAG, "Location permission denied")
@@ -80,28 +110,33 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
 
-
     private fun getCurrentLocation() {
         //checks for permission
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) !=
-            PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) !=
+            PackageManager.PERMISSION_GRANTED
+        ) {
             // requests permissions if needed
             requestLocationPermissions()
-        }
-        else {
+        } else {
             map.isMyLocationEnabled = true
             // 3
             fusedLocationClient.lastLocation.addOnCompleteListener {
                 val location = it.result
                 if (location != null) {
                     // create latlng
-                    val latLng = LatLng(location.latitude,
-                        location.longitude)
+                    val latLng = LatLng(
+                        location.latitude,
+                        location.longitude
+                    )
 
                     // cameraupdate object
-                    val update = CameraUpdateFactory.newLatLngZoom(latLng,
-                        16.0f)
+                    val update = CameraUpdateFactory.newLatLngZoom(
+                        latLng,
+                        16.0f
+                    )
                     // call moveCamera
                     map.moveCamera(update)
                 } else {
@@ -111,6 +146,94 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             }
         }
     }
+
+    private fun displayPoi(pointOfInterest: PointOfInterest) {
+        // 1
+        displayPoiGetPlaceStep(pointOfInterest)
+    }
+
+    private fun displayPoiGetPlaceStep(pointOfInterest: PointOfInterest) {
+        val placeId = pointOfInterest.placeId
+        // 2
+        val placeFields = listOf(
+            Place.Field.ID,
+            Place.Field.NAME,
+            Place.Field.PHONE_NUMBER,
+            Place.Field.PHOTO_METADATAS,
+            Place.Field.ADDRESS,
+            Place.Field.LAT_LNG
+        )
+        // 3
+        val request = FetchPlaceRequest
+            .builder(placeId, placeFields)
+            .build()
+        // 4
+        placesClient.fetchPlace(request)
+            .addOnSuccessListener { response ->
+                // 5
+                val place = response.place
+                displayPoiGetPhotoStep(place)
+            }.addOnFailureListener { exception ->
+                // 6
+                if (exception is ApiException) {
+                    val statusCode = exception.statusCode
+                    Log.e(
+                        TAG,
+                        "Place not found: " +
+                                exception.message + ", " +
+                                "statusCode: " + statusCode
+                    )
+                }
+            }
+    }
+
+    private fun displayPoiGetPhotoStep(place: Place) {
+        // grab photo from metadata of selected place
+        val photoMetadata = place.getPhotoMetadatas()?.get(0)
+        // if no photo, skip
+        if (photoMetadata == null) {
+            displayPoiDisplayStep(place, null)
+            return
+        }
+        //pass the image and size
+        val photoRequest = FetchPhotoRequest.builder(photoMetadata)
+            .setMaxWidth(resources.getDimensionPixelSize(
+                    R.dimen.default_image_width
+                )
+            )
+            .setMaxHeight(resources.getDimensionPixelSize(
+                    R.dimen.default_image_height
+                )
+            )
+            .build()
+        // 4
+        placesClient.fetchPhoto(photoRequest)
+            .addOnSuccessListener { fetchPhotoResponse ->
+                val bitmap = fetchPhotoResponse.bitmap
+                displayPoiDisplayStep(place, bitmap)
+            }.addOnFailureListener { exception ->
+                if (exception is ApiException) {
+                    val statusCode = exception.statusCode
+                    Log.e(
+                        TAG,
+                        "Place not found: " +
+                                exception.message + ", " +
+                                "statusCode: " + statusCode
+                    )
+                }
+            }
+    }
+
+    private fun displayPoiDisplayStep(place: Place, photo: Bitmap?)
+    {
+        val marker = map.addMarker(MarkerOptions()
+            .position(place.latLng as LatLng)
+            .title(place.name)
+            .snippet(place.phoneNumber)
+        )
+        marker?.tag = photo
+    }
+
 
     companion object {
         private const val REQUEST_LOCATION = 1
---
+++ app/src/main/java/com/raywenderlich/placebook/adapter/BookmarkInfoWindowAdapter.kt
@@ -0,0 +1,30 @@
+// 1
+package com.raywenderlich.placebook.adapter
+import android.app.Activity
+import android.graphics.Bitmap
+import android.view.View
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.Marker
+import com.raywenderlich.placebook.databinding.ContentBookmarkInfoBinding
+
+// 2
+class BookmarkInfoWindowAdapter(context: Activity) :
+    GoogleMap.InfoWindowAdapter {
+    // 3
+    private val binding =
+        ContentBookmarkInfoBinding.inflate(context.layoutInflater)
+    // 4
+    override fun getInfoWindow(marker: Marker): View? {
+        // This function is required, but can return null if
+        // not replacing the entire info window
+        return null
+    }
+    // 5
+    override fun getInfoContents(marker: Marker): View? {
+        binding.title.text = marker.title ?: ""
+        binding.phone.text = marker.snippet ?: ""
+        val imageView = binding.photo
+        imageView.setImageBitmap((marker.tag as Bitmap))
+        return binding.root
+    }
+}
